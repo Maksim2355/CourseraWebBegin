4decf1c1b95cfcfb6f613c4c52668a509fe86478
Maksim Provotorov <provotorov_maksim@mail.ru>
2020-09-29 21:30:46 +0300

    Завершил работу с промисами

---
+++ 4.Fourth course. JavaScript+/3/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function[]} operations
+ * @param {Function} callback
+ */
+module.exports = function (operations, callback) {
+    const promises = [];
+    for(let i=0; i < operations.length; i++)
+    {
+        const operation = operations[i];
+        const promise = new Promise(function (resolve, reject) {
+            operation(function (err, data) {
+                if (err)
+                    reject(err);
+                else
+                    resolve(data);
+            });
+        });
+        promises.push(promise);
+
+    }
+    Promise.all(promises)
+        .then(data => callback(null, data))
+        .catch(error => callback(error));
+
+};
